5a89bac3229ef2712176b01ce28f36d7b30c5fff
Franklin <writefranklyn@gmail.com>
2024-06-27 19:53:27 +0100

    style: added app bar

--- my-app/src/App/index.js
+++ my-app/src/App/index.js
@@ -3,11 +3,13 @@ import './App.css';
 import WelcomeMessage from './WelcomeMessage';
 import styled from 'styled-components';
 import AppLayout from './AppLayout';
+import AppBar from './AppBar';
 
 
 function App() {
   return (
     <AppLayout>
+      <AppBar />
       <WelcomeMessage/>
     </AppLayout>
   );
