080ea61bd19c23adddafd034121f732663c1a400
Franklin <writefranklyn@gmail.com>
2024-06-28 00:58:18 +0100

    style: styled app bar

--- my-app/src/App/AppBar.js
+++ my-app/src/App/AppBar.js
@@ -1,16 +1,42 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, {css} from 'styled-components';
+
+const Logo = styled.div`
+    font-size: 1.5em;
+`
 
 const Bar = styled.div`
     display: grid;
+    margin-bottom: 40px;
     grid-template-columns: 180px auto 100px 100px;
 `
 
+const ControlButtonEle = styled.div`
+    cursor: pointer;
+    ${props => props.active && css`
+        color: blue;
+    `}
+`
+
+function ControlButton({name, active}){
+    return (
+        <ControlButtonEle active={active}>
+            {toProperCase(name)}
+        </ControlButtonEle>
+    )
+}
+
+function toProperCase(lower){
+    return lower.charAt(0).toUpperCase() + lower.substr(1);
+}
+
 export default function(){
-    return <Bar>
-        <div> CryptoDash </div>
-        <div />
-        <div> Dashboard </div>
-        <div> Settings </div>
-    </Bar>
+    return ( 
+        <Bar>
+            <Logo> CryptoDash </Logo>
+            <div />
+            <ControlButton active name="dashboard" />
+            <ControlButton name="settings" />
+        </Bar>
+    )
 }
