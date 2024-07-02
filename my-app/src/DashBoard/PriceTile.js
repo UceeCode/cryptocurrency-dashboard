d631ebb4ea6c783fed3f23caefee89de673fa49a
Franklin <writefranklyn@gmail.com>
2024-07-02 11:46:01 +0100

    added current favorite

--- my-app/src/DashBoard/PriceTile.js
+++ my-app/src/DashBoard/PriceTile.js
@@ -1,60 +1,66 @@
 import React from "react";
 import styled, { css } from "styled-components";
 import { SelectableTile } from "../Shared/Tile";
-import { fontSize3, fontSizeBig } from "../Shared/Styles";
+import { fontSize3, fontSizeBig, greenBoxShadow } from "../Shared/Styles";
 import { CoinHeaderGridStyled } from "../Settings/CoinHeaderGrid";
+import { AppContext } from "../App/AppProvider";
 
 const JustifyRight = styled.div`
-    justify-self: right;
-`
-
-const JustifyLeft = styled.div`
-    justify-self: left;
-`
-
-const TickerPrice = styled.div`
-    ${fontSizeBig}
-    font-weight: bold;
-`
-
-const ChangePct = styled.div`
-    color: green;
-    font-weight: bold;
-    ${props => props.red && css`
-        color: red
-    `}
-`
-
-const numberFormat = number => {
-    return +(number + '').slice(0, 7);
-}
-
-const PriceTileStyled = styled(SelectableTile)`
-    ${props => props.compact && css`
-        ${fontSize3}
-        display: grid;
-        grid-template-columns: repeat(3, 1fr);
-        grid-gap: 7px;
-        justify-items: right;
-    `}
+  justify-self: right;
 `;
 
-function ChangePercent({data}){
-    return (
-        <JustifyRight>
-            <ChangePct red={data.CHANGEPCT24HOUR < 0}>
-                {numberFormat(data.CHANGEPCT24HOUR)}
-            </ChangePct>
-        </JustifyRight>
-    );
+const JustifyLeft = styled.div`
+  justify-self: left;
+`;
+
+const TickerPrice = styled.div`
+  ${fontSizeBig}
+  font-weight: bold;
+`;
+
+const ChangePct = styled.div`
+  color: green;
+  font-weight: bold;
+  ${props => props.red && css`
+    color: red;
+  `}
+`;
+
+const numberFormat = number => {
+  return +(number + '').slice(0, 7);
+};
+
+const PriceTileStyled = styled(SelectableTile)`
+  ${props => props.compact && css`
+    ${fontSize3}
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    grid-gap: 7px;
+    justify-items: right;
+  `}
+
+  ${props => props.currentFavorite && css`
+    ${greenBoxShadow}
+    pointer-events: none;
+  `}
+`;
+
+function ChangePercent({ data }) {
+  return (
+    <JustifyRight>
+      <ChangePct red={data.CHANGEPCT24HOUR < 0}>
+        {numberFormat(data.CHANGEPCT24HOUR)}
+      </ChangePct>
+    </JustifyRight>
+  );
 }
 
-function PriceTile({sym, data}) {
+function PriceTile({ sym, data, currentFavorite, setCurrentFavorite }) {
   return (
-    <PriceTileStyled>
+    <PriceTileStyled onClick={setCurrentFavorite} currentFavorite={currentFavorite}>
       <CoinHeaderGridStyled>
         <div>{sym}</div>
-        <ChangePercent data={data}/>
+        <ChangePercent data={data} />
       </CoinHeaderGridStyled>
       <TickerPrice>
         ${numberFormat(data.PRICE)}
@@ -63,24 +69,33 @@ function PriceTile({sym, data}) {
   );
 }
 
-function PriceTileCompact({sym, data}){
-    return (
-    <PriceTileStyled compact> 
-        <JustifyLeft>{sym}</JustifyLeft>
-        <ChangePercent data={data}/>
+function PriceTileCompact({ sym, data, currentFavorite, setCurrentFavorite }) {
+  return (
+    <PriceTileStyled onClick={setCurrentFavorite} compact currentFavorite={currentFavorite}>
+      <JustifyLeft>{sym}</JustifyLeft>
+      <ChangePercent data={data} />
       <div>
         ${numberFormat(data.PRICE)}
       </div>
     </PriceTileStyled>
-    )
+  );
 }
 
-export default function ({ price, index }) {
+export default function PriceTileWrapper({ price, index }) {
   let sym = Object.keys(price)[0];
   let data = price[sym]['USD'];
   let TileClass = index < 5 ? PriceTile : PriceTileCompact;
+  
   return (
-    <TileClass sym={sym} data={data}>
-    </TileClass>
+    <AppContext.Consumer>
+      {({ currentFavorite, setCurrentFavorite }) => (
+        <TileClass
+          sym={sym}
+          data={data}
+          currentFavorite={currentFavorite === sym}
+          setCurrentFavorite={() => setCurrentFavorite(sym)}
+        />
+      )}
+    </AppContext.Consumer>
   );
 }
